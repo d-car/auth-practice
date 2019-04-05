df2816ad09ffa1c6f08b272e7e4edc684e818c45
unknown <danielscarstensen@gmail.com>
2019-04-05 14:53:11 -0500

    registration working

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'process.env.MONGODB_URI || "mongodb://localhost/auth-practice'
+}
---
+++ routes/users.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+// User model
+const User = require('../models/User');
+
+// Login page
+router.get('/login', (req, res) => res.render('Login'));
+
+// Register Page
+router.get('/register', (req, res) => res.render('Register'));
+
+// Register Handle
+router.post('/register', (req, res) => {
+    const { name, email, password, password2 } = req.body;
+    let errors = [];
+
+    // Check required fields
+    if (!name || !email || !password || !password2) {
+        errors.push({ msg: 'Please enter all fields' });
+    }
+
+    // Check PW match
+    if (password != password2) {
+        errors.push({ msg: 'Passwords do not match' });
+    }
+
+    // Check PW length
+    if (password.length < 6) {
+        errors.push({ msg: 'Password must be at least 6 characters' });
+    }
+
+    // Check errors
+    if (errors.length > 0) {
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        });
+    } else {
+        // Validation Pass
+        User.findOne({ email: email })
+            .then(user => {
+                if (user) {
+                    // User exists
+                    errors.push({ msg: 'Email is already registered' })
+                    res.render('register', {
+                        errors,
+                        name,
+                        email,
+                        password,
+                        password2
+                    });
+                } else {
+                    const newUser = new User({
+                        name,
+                        email,
+                        password
+                    });
+
+                    // Hash PW
+                    bcrypt.genSalt(10, (err, salt) => 
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if (err) throw err;
+                            // Set PW to hashed
+                            newUser.password = hash;
+                            // Save user and redirect to login
+                            newUser.save()
+                                .then(user => {
+                                    res.redirect('users/login');
+                                })
+                                .catch()
+                    }))
+                }
+            })
+    }
+});
+
+module.exports = router;
